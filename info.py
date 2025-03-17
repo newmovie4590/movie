ac857c633a71f658c0554c403fcf60b732b1cdd4
newmovie4590 <newstime085@gmail.com>
2025-03-17 19:11:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,7 +25,7 @@ MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+NWW26sWtgYQwNm
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002351958646').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://newmovie45:newmovie45@newmovebot.zpv0s.mongodb.net/?retryWrites=true&w=majority&appName=newmovebot")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://ajay45:ajay45@newmovie45.zpv0s.mongodb.net/?retryWrites=true&w=majority&appName=newmovie45")
 DATABASE_NAME = environ.get('DATABASE_NAME', "newmoviebot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #---------------------------------------------------------------
